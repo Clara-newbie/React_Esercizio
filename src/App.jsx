cf83d0318dcb67549b5f8a15c574b712e8103fd3
Clara-newbie <m.clara.massaro@gmail.com>
2025-02-04 19:15:01 +0100

    svolto esercizio 'props'

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Hello from "./Hello";
 import Message from "./Message";
+import Welcome from "./Welcome";
 
 export default function App() {
+  const age = "24";
+  const name = "Clara";
+
   return (
     <div>
+      <Welcome name={name} age={age} />
+      <hr />
       <Hello />
       <Message />
       <Hello />
