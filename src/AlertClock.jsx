c4d93f3a8f8fc3c1e8ca783dd06b8270769f176a
Clara-newbie <m.clara.massaro@gmail.com>
2025-02-05 19:40:42 +0100

    aggiunto file

---
+++ src/AlertClock.jsx
@@ -0,0 +1,8 @@
+export default function AlertClock({ onClickEvent }) {
+  return (
+    <div>
+      <p>Per conoscere l'ora esatta...</p>
+      <button onClick={onClickEvent}>Clicca qui!</button>
+    </div>
+  );
+}
